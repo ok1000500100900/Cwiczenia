69ad3ee7cec19c07acf1a7390fed4efa6ad45e3a
ok1000500100900 <kaminskaoliwia32@gmail.com>
2025-01-20 13:01:58 +0100

    Cosinus
    Dodanie cosinusa + zamiana zmiennych na stałe

--- tryg1.cpp
+++ tryg1.cpp
@@ -1,16 +1,17 @@
 
 #include <iostream>
 #define PI 3.141592656589
+#define X 30
+#define EPS 0.000001
+
 double sinus(double x, double esp);
+double cosinus(double x, double eps);
 
 int main()
 {
-  double x = 30;
-  double esp = 0.000001;
-
-  double result = sinus(x, esp);
-  printf("%0.6f", result);
-
+  double result = sinus(X, EPS);
+  double result1 = cosinus(X, EPS);
+  printf("%f\n%f", result, result1);
   return 0;
 }
 
@@ -37,3 +38,26 @@ double sinus(double x, double esp)
   return val;
 }
 
+double cosinus(double x, double eps)
+{
+  x = x * PI / 180;
+  double powered = x;
+  double val = 1;
+  int den = 1;
+  int temp = 1;
+
+  while(powered / den > eps || powered / den < -eps)
+  {
+    if(temp % 4 == 0)
+      val += powered / den;
+
+    if(temp % 4 == 2)
+      val -= powered / den;
+
+    temp++;
+    powered *= x;
+    den *= temp;
+  }
+
+  return val;
+}
